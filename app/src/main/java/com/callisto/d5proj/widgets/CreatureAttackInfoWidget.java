0b1a7a7eacdc762c4e6f5efe676815189d435857
Emiliano De Santis <cribraider@hotmail.com>
2015-05-06 15:08:43 -0300

    Created some layouts for combatant gridview fragment.

---
+++ app/src/main/java/com/callisto/d5proj/widgets/CreatureAttackInfoWidget.java
@@ -0,0 +1,76 @@
+package com.callisto.d5proj.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.callisto.d5proj.R;
+
+/**
+ * Created by emiliano.desantis on 06/05/2015.
+ */
+public class CreatureAttackInfoWidget extends LinearLayout{
+    private TextView txtWeapon;
+    private TextView txtDmg;
+    private TextView txtSpecialQualities;
+
+    public CreatureAttackInfoWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray aAttrs = context
+            .obtainStyledAttributes(attrs, R.styleable.AttackInfo, 0, 0);
+
+        String weapon = aAttrs.getString(R.styleable.AttackInfo_weapon);
+        String specialQualities = aAttrs.getString(R.styleable.AttackInfo_specialQualities);
+        int dice = aAttrs.getInteger(R.styleable.AttackInfo_dice, 1);
+        int dieSize = aAttrs.getInteger(R.styleable.AttackInfo_dieSize, 6);
+        int damageBonus = aAttrs.getInteger(R.styleable.AttackInfo_damageBonus, 0);
+
+        aAttrs.recycle();
+
+        inflateLayout();
+
+        getTxtWeapon().setText(weapon);
+        getTxtSpecialQualities().setText(specialQualities);
+        getTxtDmg().setText(dice + "d" + dieSize + (damageBonus > 0 ? "+" : "-") + Math.abs(damageBonus));
+    }
+
+    private void inflateLayout() {
+        inflate(this.getContext(), getLayout(), this);
+
+        setTxtWeapon((TextView) findViewById(R.id.txtWeapon));
+        setTxtDmg((TextView) findViewById(R.id.txtDmg));
+        setTxtSpecialQualities((TextView) findViewById(R.id.txtSpecialQualities));
+
+    }
+
+    public TextView getTxtDmg() {
+        return txtDmg;
+    }
+
+    public void setTxtDmg(TextView txtDmg) {
+        this.txtDmg = txtDmg;
+    }
+
+    public TextView getTxtSpecialQualities() {
+        return txtSpecialQualities;
+    }
+
+    public void setTxtSpecialQualities(TextView txtSpecialQualities) {
+        this.txtSpecialQualities = txtSpecialQualities;
+    }
+
+    public TextView getTxtWeapon() {
+        return txtWeapon;
+    }
+
+    public void setTxtWeapon(TextView txtWeapon) {
+        this.txtWeapon = txtWeapon;
+    }
+
+    public int getLayout() {
+        return R.layout.fragment_frame_attack;
+    }
+}
